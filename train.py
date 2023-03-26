6dee034446d2d1ce8273940ca05ad8a78ae035b3
Mykola Lysenko <niels.kaizenman@gmail.com>
2023-03-26 21:03:00 +0300

    transfer learning with EfficientNet_B0

--- train.py
+++ train.py
@@ -6,6 +6,11 @@ import argparse
 import model_setup, data_setup, engine, utils
 
 import torchinfo
+import random
+
+random.seed(1)
+torch.manual_seed(1)
+torch.cuda.manual_seed(1)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--training_dir', type=str, required=True)
@@ -20,26 +25,42 @@ args = parser.parse_args()
 model_save_dir = 'model'
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-transforms = torchvision.transforms.Compose([
-  torchvision.transforms.Resize((64, 64)),
-  torchvision.transforms.ToTensor()
+weights = torchvision.models.EfficientNet_B0_Weights.DEFAULT
+
+model = torchvision.models.efficientnet_b0(weights).to(device)
+
+# freeze the model
+for param in model.parameters():
+  param.requires_grad = False
+
+final_transform = torchvision.transforms.Compose([
+  torchvision.transforms.Resize((228, 228)),
+  torchvision.transforms.ToTensor(),
+  torchvision.transforms.Normalize(
+    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+  )
 ])
 
 train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
   train_dir=args.training_dir,
   test_dir=args.testing_dir,
-  transform=transforms,
+  transform=final_transform,
   batch_size=args.batch_size
 )
 
-model = model_setup.TinyVGG(
-  inputs=3,
-  hidden=args.hidden_layers,
-  outputs=len(class_names),
-  device=device
+model.classifier = torch.nn.Sequential(
+  torch.nn.Dropout(p=0.2, inplace=True),
+  torch.nn.Linear(in_features=1280, out_features=len(class_names), bias=True)
 ).to(device)
 
-#torchinfo.summary(model, [10, 3, 64, 64])
+# torchinfo.summary(
+#   model=model,
+#   input_size=[32, 3, 224, 224],
+#   col_names=["input_size", "output_size", "num_params", "trainable"],
+#   col_width=20,
+#   row_settings=["var_names"]
+# )
+
 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 loss_fn = torch.nn.CrossEntropyLoss().to(device)
 accuracy_fn = torchmetrics.Accuracy(task='multiclass', num_classes=len(class_names)).to(device)
